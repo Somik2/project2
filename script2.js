ef90a6000ce59f4c079d131a092766424b5d7cce
Yura <romashenko22223333@gmail.com>
2020-12-29 09:44:32 +0300

    fitst commit

---
+++ script2.js
@@ -0,0 +1,31 @@
+let arr = ['plum.png','orange.bmp','apple.jpg'];
+
+
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+//var obj {};
+
+console.log(arr[2]);
+// alert("hello word!");
+console.log(4/0);
+console.log('string'*9);
+
+let person = {
+    name:"Jonh",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person.name);
+
+
+//let answer = confirm("Are you herer ?");
+// console.log(answer);
+
+let answer = prompt("Есть ли вам 18?", "Да");
+ console.log(typeof(answer));
